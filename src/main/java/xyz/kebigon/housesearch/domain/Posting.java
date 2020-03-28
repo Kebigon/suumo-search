f2b995ff96527e07431e9f88324e7845d1f10f6b
Kebigon <git@kebigon.xyz>
2020-03-28 18:00:58 +0900

    Improve the routes selected to be shown in the notification email

--- src/main/java/xyz/kebigon/housesearch/domain/Posting.java
+++ src/main/java/xyz/kebigon/housesearch/domain/Posting.java
@@ -5,63 +5,66 @@ import lombok.Data;
 @Data
 public class Posting
 {
-    /**
-     * URL
-     */
-    private final String url;
+	/**
+	 * URL
+	 */
+	private final String url;
 
-    /**
-     * Price (in yens)
-     */
-    private final Long price;
+	/**
+	 * Price (in yens)
+	 */
+	private final Long price;
 
-    /**
-     * Age (in years)
-     */
-    private final Integer age;
+	/**
+	 * Age (in years)
+	 */
+	private final Integer age;
 
-    /**
-     * Land surface (in square meters)
-     */
-    private final Double landSurface;
+	/**
+	 * Land surface (in square meters)
+	 */
+	private final Double landSurface;
 
-    /**
-     * House surface (in square meters)
-     */
-    private final Double houseSurface;
+	/**
+	 * House surface (in square meters)
+	 */
+	private final Double houseSurface;
 
-    /**
-     * Time to go to the closest station (in minutes)
-     */
-    private final Integer walkTimeToStation;
+	/**
+	 * Time to go to the closest station (in minutes)
+	 */
+	private final Integer walkTimeToStation;
 
-    /**
-     * Closest station
-     */
-    private final String station;
+	/**
+	 * Closest station
+	 */
+	private final String station;
 
-    /**
-     * Fastest route in the ones compared by spring expression
-     */
-    private Route fastestRoute;
+	/**
+	 * Fastest route in the ones compared by spring expression
+	 */
+	private Route fastestRoute;
 
-    /**
-     * Cheapest route in the ones compared by spring expression
-     */
-    private Route cheapestRoute;
+	/**
+	 * Cheapest route in the ones compared by spring expression
+	 */
+	private Route cheapestRoute;
 
-    /**
-     * Easiest route in the ones compared by spring expression
-     */
-    private Route easiestRoute;
+	/**
+	 * Easiest route in the ones compared by spring expression
+	 */
+	private Route easiestRoute;
 
-    public void updateRoutes(Route route)
-    {
-        if (fastestRoute == null || route.getTime() < fastestRoute.getTime())
-            fastestRoute = route;
-        if (cheapestRoute == null || route.getFare() < cheapestRoute.getFare())
-            cheapestRoute = route;
-        if (easiestRoute == null || route.getTransfer() < easiestRoute.getTransfer())
-            easiestRoute = route;
-    }
+	public void updateRoutes(Route route)
+	{
+		if (fastestRoute == null || route.getTime() < fastestRoute.getTime() || (route.getTime() == fastestRoute.getTime()
+				&& (route.getFare() < fastestRoute.getFare() || route.getTransfer() < fastestRoute.getTransfer())))
+			fastestRoute = route;
+		if (cheapestRoute == null || route.getFare() < cheapestRoute.getFare() || (route.getFare() == cheapestRoute.getFare()
+				&& (route.getTime() < cheapestRoute.getTime() || route.getTransfer() < cheapestRoute.getTransfer())))
+			cheapestRoute = route;
+		if (easiestRoute == null || route.getTransfer() < easiestRoute.getTransfer() || (route.getTransfer() == easiestRoute.getTransfer()
+				&& (route.getTime() < easiestRoute.getTime() || route.getFare() < easiestRoute.getFare())))
+			easiestRoute = route;
+	}
 }
